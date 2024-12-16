e36577174b67f15e8f62b227156050f6852ecb54
Aibored <aozdemir191@posta.pau.edu.tr>
2024-12-16 14:25:28 +0300

    test

--- middlewares/status.check.js
+++ middlewares/status.check.js
@@ -1,5 +1,15 @@
 const db = require('../configs/database.js');
 
-async function statusCheck (){
+async function statusCheck (id){
+ const project = db('tasks').select('*').where('project_id',id);
 
+ const statusTask = db('tasks').select('*').where('status', 'completed');
+
+ const proj = {
+  status: 'completed',
+ };
+
+ if (project.length === statusTask.length){
+const projectstatus = db('projects').where({id}).update(proj)
+ }
 }
